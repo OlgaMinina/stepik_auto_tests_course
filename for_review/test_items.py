e01b686b681099b40478eff434ea35cdc2bb691c
Olga Minina <arb.test2020@gmail.com>
2024-04-18 13:19:06 +0200

    edit test_items.py

--- for_review/test_items.py
+++ for_review/test_items.py
@@ -5,8 +5,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_Add_to_basket(browser):
     browser.get(link)
+    time.sleep(30)
     button_basket = browser.find_element(By.CSS_SELECTOR, 'button[type="submit"]')
-    time.sleep(10)
     assert button_basket.is_displayed(), "Add to basket button is not displayed on the website"
 
 
